5a987f0693a99c4e9b25ec3bf97c626484662113
Jeffrey Armstrong <jeffrey.armstrong@approximatrix.com>
2012-08-13 21:31:44 -0400

    First code commit

---
+++ gdb/gdbtk/library/editor.tcl
@@ -0,0 +1,60 @@
+# Editor
+# Copyright (C) 2001 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License (GPL) as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# ----------------------------------------------------------------------
+# Implements a set of editor commands
+# ----------------------------------------------------------------------
+
+namespace eval Editor {
+  namespace export edit
+
+  proc edit {loc_info} {
+    global external_editor_command
+
+    lassign $loc_info baseName fnName fileName lineNum addr pc
+
+    if {[info exists external_editor_command]} {
+        
+        #regsub -all -- "\\" "$fileName" "/" fileName
+        regsub -all {\\} $fileName / fileName
+    
+        set newCmd "sfspawn.exe $external_editor_command \"$fileName\" $lineNum"
+        if {[catch "exec $newCmd &" err]} {
+            tk_dialog .warn "Edit" $err error 0 Ok
+        }
+        
+      #if {[catch {uplevel \#0 "$external_editor_command edit $loc_info"} \
+	  #   err]} {
+	  #  tk_dialog .warn-sn "Edit" $err error 0 Ok
+      #}
+      return
+    }
+
+    
+
+#    set hWin [pref get gdb/editor]
+#    if {! [string compare $hWin ""]} {
+#      tk_dialog .warn "Edit" "No editor window specified" error 0 Ok
+#    }
+
+#    set newCmd "sfspawn.exe $hWin \"$fileName\" $lineNum"
+
+    # Replace %s with file name and %d with line number.
+#    regsub -all -- %s $newCmd $fileName newCmd
+#    regsub -all -- %d $newCmd $lineNum newCmd
+
+#    if {[catch "exec $newCmd &" err]} {
+#      tk_dialog .warn "Edit" $err error 0 Ok
+#    }
+  }
+}
